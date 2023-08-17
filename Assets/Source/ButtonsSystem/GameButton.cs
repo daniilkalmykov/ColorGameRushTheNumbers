66d350209e38553254c745c8842808d08da6b114
daniilkalmykov <daniilk24960@gmail.com>
2023-08-17 22:26:32 +0600

    add abstract class Button.cs

---
+++ Assets/Source/ButtonsSystem/GameButton.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Source.ButtonsSystem
+{
+    internal abstract class GameButton : MonoBehaviour
+    {
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnClick);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnClick);
+        }
+
+        protected abstract void OnClick();
+    }
+}
