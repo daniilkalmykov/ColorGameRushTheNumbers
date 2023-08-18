ff9a85cbc88e6f0afa501ad5461665a9ff70ad2c
daniilkalmykov <daniilk24960@gmail.com>
2023-08-18 21:52:50 +0600

    add CompositionOrder.cs

---
+++ Assets/Source/CompositeRoot/CompositionOrder.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Source.CompositeRoot
+{
+    internal sealed class CompositionOrder : MonoBehaviour
+    {
+        [SerializeField] private List<CellCompositeRoot> _cellCompositeRoots;
+        [SerializeField] private List<LineCompositeRoot> _lineCompositeRoots;
+        [SerializeField] private LevelCompositeRoot _levelCompositeRoot;
+
+        private void Awake()
+        {
+            foreach (var cellCompositeRoot in _cellCompositeRoots)
+                cellCompositeRoot.Compose();
+
+            foreach (var lineCompositeRoot in _lineCompositeRoots)
+                lineCompositeRoot.Compose();
+            
+            _levelCompositeRoot.Compose();
+        }
+    }
+}
