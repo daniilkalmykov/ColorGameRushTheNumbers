9b06a31bfef8bcc2ec77502b80380d6bdce91889
daniilkalmykov <daniilk24960@gmail.com>
2023-08-18 21:35:12 +0600

    add game text to line composite root

--- Assets/Source/CompositeRoot/LineCompositeRoot.cs
+++ Assets/Source/CompositeRoot/LineCompositeRoot.cs
@@ -1,22 +1,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using Source.LinesSystem;
+using Source.TextsSystem;
 using UnityEngine;
 
 namespace Source.CompositeRoot
 {
+    [RequireComponent(typeof(GameText))]
     internal sealed class LineCompositeRoot : CompositeRoot
     {
         [SerializeField] private List<CellCompositeRoot> _cellCompositeRoots;
         [SerializeField] private uint _sum;
 
+        private GameText _text;
+        
         public Line Line { get; private set; }
+
+        private void Awake()
+        {
+            _text = GetComponent<GameText>();
+        }
 
         public override void Compose()
         {
             var cells = _cellCompositeRoots.Select(cellCompositeRoot => cellCompositeRoot.Cell).ToList();
 
             Line = new Line(cells, _sum);
+
+            _text.Show(Line.Sum.ToString());
         }
     }
 }
